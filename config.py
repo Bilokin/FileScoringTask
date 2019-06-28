8952984a0295e0d42d562dd53b1705d109d29b2a
Sviatoslav Bilokin <sviatoslav.bilokin@desy.de>
2019-06-28 10:26:46 +0200

    Comments in config

--- config.py
+++ config.py
@@ -1,5 +1,13 @@
+# General configuration dictionary
 config = {
-    'AnalysisOperator':{}, 
-    'TextUIOperator':{'NEntries':10},
-    'PlotOperator':{'NEntries':10}
+    'AnalysisOperator':
+        {}, 
+    'TextUIOperator':
+        {
+            'NEntries':10 # Number of entries to print
+        },
+    'PlotOperator':
+        {
+            'NEntries':10 # Number of entries to plot
+        }
     }
